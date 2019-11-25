4be5a9c6c88e52abdabdafab858bf279e45360e1
Ivan Béthus <ivan_bethus@info-shelby.iut.bx1>
2019-11-25 18:04:45 +0100

    maj

--- Archi/S1/Exercices/boucle.sh
+++ Archi/S1/Exercices/boucle.sh
@@ -1,6 +1,18 @@
-#!/bin/bash
-
-while read numero nom
+## Début de la boucle infinie qui rappelle le rdv lorsque l'heure est arrivée ##
+while [ $nbLignes -ne 0 ]
 do
-	printf "| %30s %-30s |\n" $numero $nom
-done < telephones.dat
+    echo $! > boucle.pid
+    sleep 30
+    for i in $(seq 1 $nbLignes)
+    do
+        horaire=$(sed "${i}q;d" horaires.db | cut -d: -f1)
+        date=$(date +%H%M)
+        message=$(sed "${i}q;d" horaires.db)
+        if [ $date = $horaire ]
+        then
+        xmessage $message -buttons FERMER
+        sed -i "${i}d" horaires.db
+        nbLignes=$(< horaires.db wc -l)
+        fi
+    done
+done
